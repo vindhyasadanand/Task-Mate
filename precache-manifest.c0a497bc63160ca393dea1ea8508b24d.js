232ee74c9bc731cd44b931609644d01dcec13d94
Vindhya Hegde <Vindhyasadanand@gmail.com>
2021-05-27 19:20:05 +0530

    Updates

---
+++ precache-manifest.c0a497bc63160ca393dea1ea8508b24d.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0d259d4925f61ff9fddd0f30076bfac",
+    "url": "/Task-Mate/index.html"
+  },
+  {
+    "revision": "a6f07b8597acb3dc1650",
+    "url": "/Task-Mate/static/js/2.a6f19205.chunk.js"
+  },
+  {
+    "revision": "86fb55ec9f1a84af4652",
+    "url": "/Task-Mate/static/js/main.dca1e5b4.chunk.js"
+  },
+  {
+    "revision": "955d8b35d6ec7ad64ff4",
+    "url": "/Task-Mate/static/js/runtime-main.b62ab81a.js"
+  }
+]);
