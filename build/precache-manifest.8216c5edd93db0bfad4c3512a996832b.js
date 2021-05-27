fd8b83e43f003035d3c99ff4797594c0887c84e1
Vindhya Hegde <Vindhyasadanand@gmail.com>
2021-05-27 19:17:22 +0530

    deploy my react app to github

---
+++ build/precache-manifest.8216c5edd93db0bfad4c3512a996832b.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab6d404a1b16aa39fd641a47d904feeb",
+    "url": "/ask-Mate/index.html"
+  },
+  {
+    "revision": "a6f07b8597acb3dc1650",
+    "url": "/ask-Mate/static/js/2.a6f19205.chunk.js"
+  },
+  {
+    "revision": "86fb55ec9f1a84af4652",
+    "url": "/ask-Mate/static/js/main.dca1e5b4.chunk.js"
+  },
+  {
+    "revision": "2e509181c3ce5828e8ed",
+    "url": "/ask-Mate/static/js/runtime-main.8a96a5a7.js"
+  }
+]);
